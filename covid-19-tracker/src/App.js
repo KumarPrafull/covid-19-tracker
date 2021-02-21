265930caa59f82dfa9593a69ad9e8f03457651cf
Kumar Prafull <eprafull@gmail.com>
2021-02-21 21:09:17 +0530

    first commit

---
+++ covid-19-tracker/src/App.js
@@ -0,0 +1,39 @@
+import React from "react";
+import {
+  MenuItem,
+  FormControl,
+  Select,
+} from "@material"
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>COVID 19 TRACKER</h1>
+      <FormControl className="app_dropdown">
+        <Select variant ="outline" value ="abs">
+          <menuItem value = "worldwide"> Worldwide</menuItem>
+          <menuItem value = "worldwide"> Worldwide</menuItem>
+          <menuItem value = "worldwide"> Worldwide</menuItem>
+          <menuItem value = "worldwide"> Worldwide</menuItem>
+
+
+        </Select>
+
+      </FormControl>
+     {/* Header */}
+     {/* Title + select input dropdown field */}
+     
+     {/* InfoBox*/}
+     {/* InfoBox*/}
+     {/* InfoBox*/}
+
+     {/* Table */}
+     {/* Graph */}
+
+     {/* Map */}
+    </div>
+  );
+}
+
+export default App;
