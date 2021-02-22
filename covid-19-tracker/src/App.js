9786541a9dda3e536808b02c6ac9b1c28f76264d
Kumar Prafull <eprafull@gmail.com>
2021-02-22 17:32:04 +0530

    setCountry

--- covid-19-tracker/src/App.js
+++ covid-19-tracker/src/App.js
@@ -8,6 +8,7 @@ import './App.css';
 
 function App() {
   const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState("worldwide");
   useEffect(() => {
     const getCountreisData = async () => {
       await fetch("https://disease.sh/v3/covid-19/countries")
@@ -24,15 +25,23 @@ function App() {
     getCountreisData();
   },[]);
 
+
+
+  const onCountryChange = async (event) => {
+    const countryCode = event.target.value;
+    console.log("YAY",countryCode);
+    
+    setCountry(countryCode);
+  };
+
   return (
     <div className="app">
       <div className="app__header">
       <h1>COVID 19 TRACKER</h1>
       <FormControl className="app__dropdown">
-        <Select variant ="outlined" value ="abc">
-
-          {
-            countries.map(country => (
+        <Select variant ="outlined" onChange={onCountryChange} value ={country}>
+        <MenuItem value="worldwide">Worldwide</MenuItem>
+          {countries.map(country => (
               <MenuItem value={country.value}>{country.name}</MenuItem>              
 
             ))}
